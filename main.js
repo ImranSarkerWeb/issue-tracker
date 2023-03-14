a56c734f4786f12272f9bdfbd98a5cde8fdb9ee4
imransarkerweb <imransarker.web@gmail.com>
2023-03-14 14:07:32 +0600

    fix some issues

--- main.js
+++ main.js
@@ -23,7 +23,8 @@ function submitIssue(e) {
 
 const closeIssue = id => {
   const issues = JSON.parse(localStorage.getItem('issues'));
-  const currentIssue = issues.find(issue => issue.id === id);
+
+  const currentIssue = issues.find(issue =>  issue.id == id);
   currentIssue.status = 'Closed';
   localStorage.setItem('issues', JSON.stringify(issues));
   fetchIssues();
@@ -31,7 +32,7 @@ const closeIssue = id => {
 
 const deleteIssue = id => {
   const issues = JSON.parse(localStorage.getItem('issues'));
-  const remainingIssues = issues.filter( issue.id !== id )
+  const remainingIssues = issues.filter( issues.id !== id )
   localStorage.setItem('issues', JSON.stringify(remainingIssues));
 }
 
@@ -40,17 +41,19 @@ const fetchIssues = () => {
   const issuesList = document.getElementById('issuesList');
   issuesList.innerHTML = '';
 
-  for (var i = 0; i < issues.length; i++) {
-    const {id, description, severity, assignedTo, status} = issues[i];
-
-    issuesList.innerHTML +=   `<div class="well">
-                              <h6>Issue ID: ${id} </h6>
-                              <p><span class="label label-info"> ${status} </span></p>
-                              <h3> ${description} </h3>
-                              <p><span class="glyphicon glyphicon-time"></span> ${severity}</p>
-                              <p><span class="glyphicon glyphicon-user"></span> ${assignedTo}</p>
-                              <a href="#" onclick="setStatusClosed(${id})" class="btn btn-warning">Close</a>
-                              <a href="#" onclick="deleteIssue(${id})" class="btn btn-danger">Delete</a>
-                              </div>`;
-  }
+if(issues){
+    for (var i = 0; i < issues.length; i++) {
+        const {id, description, severity, assignedTo, status} = issues[i];
+    
+        issuesList.innerHTML +=   `<div class="well">
+                                  <h6>Issue ID: ${id} </h6>
+                                  <p><span class="label label-info"> ${status} </span></p>
+                                  <h3> ${description} </h3>
+                                  <p><span class="glyphicon glyphicon-time"></span> ${severity}</p>
+                                  <p><span class="glyphicon glyphicon-user"></span> ${assignedTo}</p>
+                                  <a href="#" onclick="closeIssue(${id})" class="btn btn-warning">Close</a>
+                                  <a href="#" onclick="deleteIssue(${id})" class="btn btn-danger">Delete</a>
+                                  </div>`;
+      }
+}
 }
